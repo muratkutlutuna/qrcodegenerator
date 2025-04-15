c360b0c1ffc390a235f94d5d4e780e3d8dfccf3d
muratkutlutuna <muratkutlutuna@gmail.com>
2025-04-15 12:35:12 +0200

    functions_collection_types_LoopsWS_ConditionlaWS

---
+++ self_study/collectionTypes/workshop.py
@@ -0,0 +1,4 @@
+list1 = [(2019, 'Freshman'), (2020, 'Sophomore'), (2021, 'Junior'), (2022, 'Senior')]
+college_years = ['Freshman', 'Sophomore', 'Junior', 'Senior']
+print(list(enumerate(college_years, 2019)))
+print(list(zip((1,2,3,4), (5,6,7,8))))
